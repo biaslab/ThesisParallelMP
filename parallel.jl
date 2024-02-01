bf5db1987e9d605a62c3641212dc08ef3e16bd5d
Mattia-Vicari <mattia.vic99@gmail.com>
2024-02-01 16:58:09 +0100

    create benchmark of AR vs rule time

--- parallel.jl
+++ parallel.jl
@@ -1,9 +1,29 @@
-using RxInfer
+using RxInfer, BenchmarkTools
 
 # A `PromisedMessage` holds a promise of the message computation
 # Use `as_message` function to block and wait for the result.
 struct PromisedMessage{T} <: ReactiveMP.AbstractMessage
 	task::T
+end
+
+# Split the mapreduce work over the available threads.
+function parallelmapreduce(f, op, x)
+	if length(x) < Threads.nthreads() * 2
+		return mapreduce(f, op, x)
+	end
+
+	results = Array{eltype(x)}(undef, Threads.nthreads())
+	Threads.@threads for tid in 1:Threads.nthreads()
+		len = div(length(x), Threads.nthreads())
+
+		if tid == 1:Threads.nthreads()
+			len += length(x) % Threads.nthreads()
+		end
+
+		domain = ((tid-1)*len +1):tid*len
+		results[tid] = mapreduce(f, op, view(x, domain))
+	end
+	reduce(op, results)
 end
 
 # The `as_message` for the `PromisedMessage` blocks until available
@@ -18,9 +38,48 @@ function as_promised(event)
 	return PromisedMessage(task)
 end
 
-function custom_prod(strategy, _, _)
+function blocking_parallel_prod(strategy, _, _)
 	× = (left, right) -> ReactiveMP.multiply_messages(strategy, left, right)
-	return (messages) -> mapreduce(as_message, ×, messages)
+	return (messages) -> parallelmapreduce(as_message, ×, messages)
+end
+
+function done_first_parallel_prod(strategy, _, _)
+	× = (left, right) -> ReactiveMP.multiply_messages(strategy, left, right)
+	return (messages) -> begin
+	 	ispromised = map((msg)->msg isa PromisedMessage, messages)
+		variational = view(messages, .!ispromised)
+		promised = view(messages, ispromised)
+		isdone = map(istaskdone, getfield.(promised, :task))
+		done = view(promised, isdone)
+		notdone = view(promised, .!isdone)
+		first = true
+		toreduce = vcat(variational, done)
+
+		if length(toreduce) > 0
+			res = parallelmapreduce(as_message, ×, toreduce)
+			first = false
+		end
+
+		while length(notdone) > 0
+			isdone = map(istaskdone, getfield.(notdone, :task))
+			done = view(notdone, isdone)
+			notdone = view(notdone, .!isdone)
+			if first & length(done) > 0
+				res = parallelmapreduce(as_message, ×, vcat(variational, done))
+				first = false
+			else
+				if length(done) > 0
+					res = vcat(res, parallelmapreduce(as_message, ×, done))
+				end
+			end
+		end
+
+		if res isa Message
+			return res
+		end
+
+		return reduce(×, res)
+	end
 end
 
 function after_iteration_cb(model, iteration)
